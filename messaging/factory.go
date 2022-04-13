397650c09cf9ccb5c8f9825d52be418a0b2988fc
Leonard Goodell <leonard.goodell@intel.com>
2022-04-13 13:52:16 -0700

    feat: Add build flag to build w/o messging capability
    Signed-off-by: Leonard Goodell <leonard.goodell@intel.com>

--- messaging/factory.go
+++ messaging/factory.go
@@ -1,5 +1,8 @@
+//go:build !no_messagebus
+// +build !no_messagebus
+
 //
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2021 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
