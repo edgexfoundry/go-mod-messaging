1a5f5cf7ee1694f85f5961da7ec7b42515e04c2b
Alex Ullrich <alex.ullrich@gmail.com>
2023-10-13 13:31:57 -0400

    test: Remove JetStream Integration Tests
    These tests used an embedded server and brought an unwanted dependency
    on the nats-server package.  Fixes #284
    
    Signed-off-by: Alex Ullrich <alex.ullrich@gmail.com>

--- internal/pkg/nats/jetstream/client_test.go
+++ internal/pkg/nats/jetstream/client_test.go
@@ -19,126 +19,13 @@
 package jetstream
 
 import (
-	"context"
 	"reflect"
 	"testing"
-	"time"
 
-	"github.com/edgexfoundry/go-mod-messaging/v3/internal/pkg"
-	natsMessaging "github.com/edgexfoundry/go-mod-messaging/v3/internal/pkg/nats"
-	"github.com/edgexfoundry/go-mod-messaging/v3/pkg/types"
 	"github.com/google/uuid"
-	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
 	"github.com/stretchr/testify/assert"
 )
-
-func Test_autoProvision(t *testing.T) {
-	for _, tc := range []struct {
-		name          string
-		streamSubject string
-		durable       string
-		expectStream  string
-	}{
-		{"subject only", "teststream1", "", "teststream1"},
-		{"durable", "teststream2", "named", "named"},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			srvr, err := server.NewServer(&server.Options{JetStream: true, StoreDir: t.TempDir()})
-			assert.NoError(t, err)
-
-			srvr.Start()
-			defer srvr.Shutdown()
-
-			client, err := nats.Connect(srvr.ClientURL(), nil)
-			assert.NoError(t, err)
-
-			js, err := client.JetStream()
-			assert.NoError(t, err)
-
-			config := natsMessaging.ClientConfig{
-				ClientOptions: natsMessaging.ClientOptions{
-					Subject: tc.streamSubject,
-					Durable: tc.durable,
-				},
-			}
-
-			assert.NoError(t, autoProvision(config, js))
-
-			_, err = js.StreamInfo(tc.expectStream)
-			assert.NoError(t, err)
-
-			// should be able to re-run without error
-			assert.NoError(t, autoProvision(config, js))
-		})
-	}
-}
-
-func Test_ExactlyOnce(t *testing.T) {
-	port := 4223
-	srvr, err := server.NewServer(&server.Options{JetStream: true, StoreDir: t.TempDir(), Port: port})
-	assert.NoError(t, err)
-
-	srvr.Start()
-	defer srvr.Shutdown()
-
-	client, err := NewClient(types.MessageBusConfig{
-		Broker: types.HostInfo{
-			Host:     "localhost",
-			Port:     port,
-			Protocol: "nats",
-		},
-		Type: "nats-jetstream",
-		Optional: map[string]string{
-			pkg.ExactlyOnce:   "true",
-			pkg.AutoProvision: "true",
-			pkg.Subject:       "edgex/#",
-		},
-	})
-
-	assert.NoError(t, err)
-
-	assert.NoError(t, client.Connect())
-
-	topic := "edgex/#"
-	messages := make(chan types.MessageEnvelope)
-	errors := make(chan error)
-
-	err = client.Subscribe([]types.TopicChannel{{
-		Topic:    topic,
-		Messages: messages,
-	}}, errors)
-
-	m0 := types.MessageEnvelope{CorrelationID: uuid.NewString(), Payload: []byte(uuid.NewString())}
-	m1 := types.MessageEnvelope{CorrelationID: uuid.NewString(), Payload: []byte(uuid.NewString())}
-
-	ctx, done := context.WithTimeout(context.Background(), 30*time.Second)
-
-	go func() {
-		assert.NoError(t, client.Publish(m0, topic))
-		assert.NoError(t, client.Publish(m0, topic))
-		assert.NoError(t, client.Publish(m1, topic))
-		assert.NoError(t, client.Publish(m1, topic))
-		assert.NoError(t, client.Publish(m0, topic))
-		assert.NoError(t, client.Publish(m1, topic))
-
-		done()
-	}()
-
-	received := make([]types.MessageEnvelope, 0)
-
-	for {
-		select {
-		case m := <-messages:
-			received = append(received, m)
-		case <-ctx.Done():
-			assert.Equal(t, 2, len(received))
-			assert.Equal(t, m0.CorrelationID, received[0].CorrelationID)
-			assert.Equal(t, m1.CorrelationID, received[1].CorrelationID)
-			return
-		}
-	}
-}
 
 func Test_parseDeliver(t *testing.T) {
 	tests := []struct {
